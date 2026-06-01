bc7b2bed89fb3f0fc0d3bc8e193d11a893b87104
oscar <oscarjoelchoque@gmail.com>
2026-06-01 18:48:14 -0400

    Agregar variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AuthModule } from './auth/auth.module';
 import { RolModule } from './rol/rol.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'auxorm',
-    autoLoadEntities: true,
-    synchronize: true,
-    //dropSchema: true
-
-  }), UsuarioModule, AuthModule, RolModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: 'localhost', //config.get('DB_HOST)
+        port: 3306, //config.get<number>('DB_PORT')
+        username: 'root', //config.get('DB_USERNAME')
+        password: '', //config.get('DB_PASSWORD')
+        database: 'auxorm', //config.get('DB_DATABASE')
+        autoLoadEntities: true,
+        synchronize: true,
+        //dropSchema: true
+      }),
+      inject: [ConfigService]
+    }),
+    UsuarioModule,
+    AuthModule,
+    RolModule],
   controllers: [AppController],
   providers: [AppService],
 })
